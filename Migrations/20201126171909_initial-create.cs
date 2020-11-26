3451f18cc5a002838b9a2d177bf251278d0c86ab
Aron Gimesi <aron.gimesi@avolantis.net>
2020-11-26 19:09:11 +0100

    mysql connection, docker, bugzilla db context, nuget packages

---
+++ Migrations/20201126171909_initial-create.cs
@@ -0,0 +1,99 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using MySql.Data.EntityFrameworkCore.Metadata;
+
+namespace bugzilla.Migrations
+{
+    public partial class initialcreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Bugs",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("MySQL:ValueGenerationStrategy", MySQLValueGenerationStrategy.IdentityColumn),
+                    DevId = table.Column<int>(nullable: false),
+                    Closed = table.Column<bool>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Bugs", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Developers",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("MySQL:ValueGenerationStrategy", MySQLValueGenerationStrategy.IdentityColumn),
+                    RoleId = table.Column<int>(nullable: false),
+                    Name = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Developers", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Fixes",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("MySQL:ValueGenerationStrategy", MySQLValueGenerationStrategy.IdentityColumn),
+                    BugId = table.Column<int>(nullable: false),
+                    DevId = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Fixes", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Reviews",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("MySQL:ValueGenerationStrategy", MySQLValueGenerationStrategy.IdentityColumn),
+                    FixId = table.Column<int>(nullable: false),
+                    DevId = table.Column<int>(nullable: false),
+                    Approved = table.Column<bool>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Reviews", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Roles",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("MySQL:ValueGenerationStrategy", MySQLValueGenerationStrategy.IdentityColumn),
+                    Name = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Roles", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Bugs");
+
+            migrationBuilder.DropTable(
+                name: "Developers");
+
+            migrationBuilder.DropTable(
+                name: "Fixes");
+
+            migrationBuilder.DropTable(
+                name: "Reviews");
+
+            migrationBuilder.DropTable(
+                name: "Roles");
+        }
+    }
+}
---
+++ Models/BugzillaDbContext.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace bugzilla.Models
+{
+    public class BugzillaDbContext : DbContext
+    {
+        public BugzillaDbContext(DbContextOptions<BugzillaDbContext> options):base(options)
+        {
+                
+        }
+        
+        private DbSet<Developer> Developers { get; set; }
+        private DbSet<Role> Roles { get; set; }
+        private DbSet<Bug> Bugs { get; set; }
+        private DbSet<Fix> Fixes { get; set; }
+        private DbSet<Review> Reviews { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseMySQL("server=localhost;port=3306;database=bugzilla;user=root;password=helloworld");
+        }
+
+        /* 
+           protected override void OnModelCreating(ModelBuilder modelBuilder)
+           {
+               base.OnModelCreating(modelBuilder);
+    
+               modelBuilder.Entity<Developer>(entity =>
+               {
+                   entity.HasKey(e => e.Id);
+                   entity.Property(e => e.Name).IsRequired();
+                   entity.Property(e => e.RoleId).IsRequired();
+               });
+    
+               modelBuilder.Entity<Role>(entity =>
+               {
+                   entity.HasKey(e => e.Id);
+                   entity.Property(e => e.Name).IsRequired();
+               });    
+               
+               modelBuilder.Entity<Bug>(entity =>
+               {
+                   entity.HasKey(e => e.Id);
+                   entity.Property(e => e.DevId).IsRequired();
+                   entity.Property(e => e.Closed);
+               }); 
+               
+               modelBuilder.Entity<Fix>(entity =>
+               {
+                   entity.HasKey(e => e.Id);
+                   entity.Property(e => e.DevId).IsRequired();
+                   entity.Property(e => e.BugId).IsRequired();
+               });
+               
+               modelBuilder.Entity<Review>(entity =>
+               {
+                   entity.HasKey(e => e.Id);
+                   entity.Property(e => e.DevId).IsRequired();
+                   entity.Property(e => e.FixId).IsRequired();
+                   entity.Property(e => e.Approved).IsRequired();
+               });
+           }*/
+    }
+}
